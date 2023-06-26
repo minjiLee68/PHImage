f58bae3ca553b1a3b4dbf27ca6219be3ab8b7ab9
minji0608 <88191880+minjiLee68@users.noreply.github.com>
2023-06-26 15:22:19 +0900

    Initial Commit

---
+++ photoSnapshot/photoSnapshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoSnapshotApp.swift
+//  photoSnapshot
+//
+//  Created by 이민지 on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct photoSnapshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
